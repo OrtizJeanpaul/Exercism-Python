c0dce11680999d0b65c144212a41e84c0a97a1d5
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-22 18:41:45 -0400

    Solution to the problem ARMSTRONG-NUMBERS

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -2,4 +2,10 @@ def is_armstrong_number(number):
     if 0 <= number < 10:
         return True
 
-    
+    digits = []
+    num = number
+    while num > 0:
+        digits.append(num%10)
+        num // 10
+
+    return sum((digits**len(digits)))
