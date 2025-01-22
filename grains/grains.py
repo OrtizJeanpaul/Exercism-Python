c7101093f879dca6363672182ebe66921ecf94c5
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-22 17:50:36 -0400

    Solution to the problem GRAINS

--- grains/grains.py
+++ grains/grains.py
@@ -1,9 +1,7 @@
 def square(number):
     if number < 1 or number > 64:
         raise ValueError("square must be between 1 and 64")
-    if(number == 1):
-        return 1
-    return pow(2,number)
+    return pow(2,number-1)
 
 def total():
-    return square(64)
+    return pow(2,64)
