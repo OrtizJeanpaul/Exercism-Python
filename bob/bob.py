0f25fb45ce78ef58f7a20fa88771f46a5944be09
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-25 01:58:46 -0400

    Solution to the problem BOB

--- bob/bob.py
+++ bob/bob.py
@@ -1,21 +1,12 @@
 def response(hey_bob):
-    if(hey_bob == ""):
+    if hey_bob == "":
         return "Fine. Be that way!"
 
-    is_question = hey_bob[-1] == '?'
-    hey_bob_letters = hey_bob.replace(" ","").strip()
-    is_upper = True
-
-    for index in range(len(hey_bob_letters)):
-        if not hey_bob_letters[index].isupper():
-            is_upper = False
-            break
-
-    if(is_question):
-        if(is_upper):
+    if hey_bob[-1] == '?':
+        if hey_bob.isupper():
             return "Calm down, I know what I'm doing!"
         return "Sure."
-    if(is_upper and hey_bob[-1] == "!"):
+    if hey_bob.isupper():
         return "Whoa, chill out!"
 
     return "Whatever."
