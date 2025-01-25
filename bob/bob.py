866a1d6f36330b7bbccbeb5eebb134ede6fa200b
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-25 01:56:19 -0400

    Solution to the problem BOB

--- bob/bob.py
+++ bob/bob.py
@@ -15,7 +15,23 @@ def response(hey_bob):
         if(is_upper):
             return "Calm down, I know what I'm doing!"
         return "Sure."
-    if(is_upper):
+    if(is_upper and hey_bob[-1] == "!"):
         return "Whoa, chill out!"
 
     return "Whatever."
+
+
+
+#     **"Sure."**
+#   This is his response if you ask him a question, such as "How are you?"
+#   The convention used for questions is that it ends with a question mark.
+# - **"Whoa, chill out!"**
+#   This is his answer if you YELL AT HIM.
+#   The convention used for yelling is ALL CAPITAL LETTERS.
+# - **"Calm down, I know what I'm doing!"**
+#   This is what he says if you yell a question at him.
+# - **"Fine. Be that way!"**
+#   This is how he responds to silence.
+#   The convention used for silence is nothing, or various combinations of whitespace characters.
+# - **"Whatever."**
+#   This is what he answers to anything else.
