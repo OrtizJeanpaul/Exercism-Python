c9f176d28db7cc1be643da16258351aef68bb8d2
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-27 00:26:13 -0400

    Solution to the problem DARTS

--- darts/darts.py
+++ darts/darts.py
@@ -2,10 +2,10 @@ def score(x, y):
     if 0 <= x <=1  and 0 <= y <= 1:
         return 10
     
-    if  x <= 5 and  y <= 5:
+    if 0 <= x <= 5 and 2 <= y <= 5:
         return 5
     
-    if  x <= 10 and  y <= 10:
+    if 0 <= x <= 10 and 0 <= y <= 10:
         return 1
 
-    return 0
+    return 
