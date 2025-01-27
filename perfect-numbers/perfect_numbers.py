8f685b60ddcfd5158a1c16f70c2ff77519bd810e
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-27 02:31:09 -0400

    Solution to the problem PERFECT-NUMBERS

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -16,13 +16,13 @@ def classify(number):
     if factor_sum == number:
         return "perfect"
 
-    return "abundant" if number > factor_sum else "deficient"
+    return "abundant" if number < factor_sum else "deficient"
 
 def factors(number):
     half = number // 2
     factor_list = []
     for index in range(1,half+1):
-        if number % index:
+        if number % index == 0:
             factor_list.append(index)
     
     return factor_list
