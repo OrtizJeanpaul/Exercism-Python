8abe7180e3b89b99404771faa209562d3bde7897
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-28 00:46:33 -0400

    Solution to the problem REVERSE-STRING

--- reverse-string/reverse_string.py
+++ reverse-string/reverse_string.py
@@ -1,9 +1,11 @@
 def reverse(text):
-    char_list = list(text)
-    reversed_word = ""
-    index = len(char_list) - 1
-    while index >= 0:
-        reversed_word += char_list[index]
-        index-=1
 
-    return reversed_word
+    return text[::-1]
+    # char_list = list(text)
+    # reversed_word = ""
+    # index = len(char_list) - 1
+    # while index >= 0:
+    #     reversed_word += char_list[index]
+    #     index-=1
+
+    # return reversed_word
