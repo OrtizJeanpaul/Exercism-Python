f50b95dcea478387bc78f5d08e9c59fd9fcdb6ff
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-01-29 22:47:23 -0400

    Solution to the problem PANGRAM

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,8 +1,8 @@
 def is_pangram(sentence):
-    sentence.tolower().replace(' ','')
+    sentence.replace(' ','')
     letter_set = set()
     for letter in sentence:
-        if letter.isalpha():
+        if letter.tolower().isalpha():
             letter_set.add(letter)
     return len(letter_set) == 26
     
