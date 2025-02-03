a7dea2b69ce4a911901f92e7ba6c66f5938d51ba
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-02-03 02:39:18 -0400

    Solution to the problem ISBN-VERIFIER

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,16 +1,16 @@
 def is_valid(isbn):
     isbn = isbn.replace("-","")
-
     index = 0
     total = 0
     allowed_chars = "0123456789xX"
     while index <10:
         temp = isbn[index]
         if(allowed_chars.find(temp) != -1):
-            if(temp.lower() == "x"):
+            if(temp.lower() == "x" and index == 9):
                 temp = 10
             total += (int(temp) * (10 - index))
             index+=1
         else:
             return False
     return total % 11 == 0
+    
