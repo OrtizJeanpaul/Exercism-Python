7b7a61aba279d9a4b65db4ea9fdbc1bfdd4e7116
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-02-02 21:24:24 -0400

    Solution to the problem ISOGRAM

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,5 +1,5 @@
 def is_isogram(string):
-    just_letter_string = string.replace(" ", "").replace("-","")
+    just_letter_string = string.replace(" ", "").replace("-","").lower()
     word_set = set(just_letter_string)
 
     return len(word_set) == len(just_letter_string)
