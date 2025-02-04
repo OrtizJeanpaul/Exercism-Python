ad7f34de2160c50a60ceca49495ad7414184e072
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-02-04 15:06:18 -0400

    Solution to the problem ROTATIONAL-CIPHER

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -2,14 +2,12 @@ from string import ascii_lowercase
 
 def rotate(text, key):
     char_list = ascii_lowercase
-    rotation_value = key.replace("ROT","")
-    rotation_value = int(rotation_value)
     mapped_chars = dict()
     
 
     for index in len(char_list):
         temp = char_list[index]
-        mapped_chars[temp] = char_list[(rotation_value + index)%26]
+        mapped_chars[temp] = char_list[(key + index)%26]
     
     result = ""
     for char in enumerate(text):
