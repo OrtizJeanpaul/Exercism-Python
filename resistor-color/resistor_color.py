3dabdd43c42c6fee7d29f90e01a42b7dac3306f2
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-02-05 02:56:25 -0400

    Solution to the problem RESISTOR-COLOR

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,10 @@
+def color_code(color):
+    color_array = colors()
+
+    return color_array.index(color)
+
+
+def colors():
+    return ["black", "brown", "red", 
+            "orange", "yellow", "green", 
+            "blue","violet", "grey","white"]
