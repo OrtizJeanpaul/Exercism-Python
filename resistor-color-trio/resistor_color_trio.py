c1078a62d4520ac1d51d8ac513e2d9a48c993a5b
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-02-05 03:28:16 -0400

    Solution to the problem RESISTOR-COLOR-TRIO

--- resistor-color-trio/resistor_color_trio.py
+++ resistor-color-trio/resistor_color_trio.py
@@ -3,12 +3,10 @@ def label(colors):
     COLOR_ARRAY = ["black", "brown", "red", "orange", "yellow", "green", "blue","violet", "grey","white"]
 
     res = 10*COLOR_ARRAY.index(colors[0]) + COLOR_ARRAY.index(colors[1])
-    num_of_zeros = math.pow(10, COLOR_ARRAY.index(colors[2]))
-
+    num_of_zeros = 10**COLOR_ARRAY.index(colors[2])
 
     res = res * num_of_zeros
     prefix = " "
-
 
     if res % 1000000000 != res:
         res = res / 1000000000
