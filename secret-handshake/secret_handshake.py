b70332a89df88c2e4a07c6c4e7dbfbcc8d370a17
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-02-10 02:08:54 -0400

    Solution to the problem SECRET-HANDSHAKE

---
+++ secret-handshake/secret_handshake.py
@@ -0,0 +1,13 @@
+def commands(binary_str):
+    ACTIONS = ["Reverse","jump","close your eyes","double blink","wink"]
+
+    binary_list = [binary_str]
+    should_reverse = True
+    handshake = list()
+    for index in range(len(binary_list)):
+        if binary_list[index] == 1:
+            temp = ACTIONS[index]
+            if(temp == "Reverse"):
+                should_reverse = False
+            else:
+                handshake.append(ACTIONS[index])
