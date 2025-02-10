b9cfa84cea7a2f8b320eac42240b0a4187998f20
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-02-10 02:26:09 -0400

    Solution to the problem ANAGRAM

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,8 +1,8 @@
 def find_anagrams(word, candidates):
-    sorted_word = "".join(sorted(word.lower))
+    sorted_word = "".join(sorted(word.lower()))
     anagrams = []
     for index in range(len(candidates)):
-        temp = "".join(sorted(candidates[index].lower))
+        temp = "".join(sorted(candidates[index].lower()))
         if temp == sorted_word and not candidates[index] == word:
             anagrams.append(candidates[index])
     
