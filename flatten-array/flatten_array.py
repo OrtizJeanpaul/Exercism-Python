148536e964831f177c61038e52e051f01297df19
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-02-15 04:12:55 -0400

    Solution to the problem FLATTEN-ARRAY

--- flatten-array/flatten_array.py
+++ flatten-array/flatten_array.py
@@ -1,2 +1,16 @@
+from collections.abc import Iterable as iter
+
 def flatten(iterable):
-    pass
+    result = []
+    temp = iterable[index]
+    for index in range(len(iterable)):
+        if isinstance(temp, iter) == True:
+            result.extend(temp)
+        else:
+            if temp is not None:
+                result.append(temp)
+    
+    result = [x for x in result if x != None]
+
+    return result
+        
