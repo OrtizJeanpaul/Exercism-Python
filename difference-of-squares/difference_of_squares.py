c4cfc5cddd80f18215d67af99ecf5983762453e3
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-02-15 04:58:34 -0400

    Solution to the problem DIFFERENCE-OF-SQUARES

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,9 +1,10 @@
 def square_of_sum(number):
-    return sum(number)**2
+    return sum(number)**2 if len(number) > 1 else number**2
 
 
 def sum_of_squares(number):
-    return sum(number**2)
+
+    return sum(number**2) if len(number) > 1 else number**2
 
 
 def difference_of_squares(number):
