0f688550839831659453f4b4df75e19b5358bfab
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-02-15 05:10:25 -0400

    Solution to the problem DIFFERENCE-OF-SQUARES

--- difference-of-squares/difference_of_squares.py
+++ difference-of-squares/difference_of_squares.py
@@ -1,8 +1,9 @@
 def square_of_sum(number):
     if number == 0:
         return 0
-    return number + square_of_sum(number-1)
+    result = number + square_of_sum(number-1)
 
+    return result**2
 
 def sum_of_squares(number):
     if number == 0:
@@ -11,4 +12,4 @@ def sum_of_squares(number):
 
 
 def difference_of_squares(number):
-    return square_of_sum(number)**2 - sum_of_squares(number) 
+    return square_of_sum(number) - sum_of_squares(number) 
