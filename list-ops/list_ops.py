b816c9d4ce1e613d7546f8e7d157937daf9ed308
OrtizJeanpaul <jeanpaulortiz13@gmail.com>
2025-02-17 02:53:39 -0400

    Solution to the problem LIST-OPS

---
+++ list-ops/list_ops.py
@@ -0,0 +1,44 @@
+def append(list1, list2):
+    for item in enumerate(list2):
+        list1.__add__(item)
+
+    return list1
+
+def concat(lists):
+    result = []
+    for sList in enumerate(lists):
+        result.extend(sList)
+
+    return result
+
+def filter(function, list):
+    return
+
+def length(list):
+    i = 0
+    for index in list:
+        i = index
+
+    return i+1
+
+
+def map(function, list):
+    return
+
+def foldl(function, list, initial):
+    pass
+
+
+def foldr(function, list, initial):
+    pass
+
+
+def reverse(list):
+    result = []
+    index = length(list) - 1
+    
+    while index >= 0:
+        append(result, [list[index]])
+        index-=1
+    
+    return result
